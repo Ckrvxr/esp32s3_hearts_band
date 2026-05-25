0ac92585bd2b16fc8c570d0b0ac7aff3266e9bc3
Ckrvxr <85326108+Ckrvxr@users.noreply.github.com>
2026-05-25 17:34:23 +0800

    Refactor main.c to implement MainTask for periodic logging and update settings.json with flash type configuration

--- main/main.c
+++ main/main.c
@@ -1,6 +1,21 @@
+#include "esp_log.h"
+
 #include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static const char *TAG = "RTOS";
+
+static void MainTask(void *pvParameters)
+{
+    while (1) {
+        ESP_LOGI(TAG, "System running.");
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
 
 void app_main(void)
 {
-
+    xTaskCreatePinnedToCore(MainTask, "MainTask", 4096, NULL, 1, NULL, tskNO_AFFINITY);
 }
